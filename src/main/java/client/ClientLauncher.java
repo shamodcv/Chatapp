a0aa37e61d261485c6a4625d43ce227bad3e5c33
shamodcv <shamoddilanka77@gmail.com>
2023-07-13 22:44:35 +0530

    second commit

--- src/main/java/client/ClientLauncher.java
+++ src/main/java/client/ClientLauncher.java
@@ -1,26 +1,32 @@
 package client;
 
+import controller.ClientFormController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ClientLauncher extends Application {
-
     public static void main(String[] args) {
-
         launch(args);
     }
-    @Override
-    public void start(Stage stage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/client_form.fxml"));
-        stage.setScene(new Scene(parent));
-        stage.setTitle("Client");
-        stage.centerOnScreen();
 
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ClientForm.fxml"));
+        ClientFormController controller = new ClientFormController();
+        fxmlLoader.setController(controller);
+        primaryStage.setScene(new Scene(fxmlLoader.load()));
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(primaryStage.getScene().getWindow());
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        stage.centerOnScreen();
+        stage.setResizable(false);
         stage.show();
     }
 }
